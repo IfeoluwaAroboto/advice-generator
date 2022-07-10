272cee735c7ccb2f2b2ed743765538e7954c9300
IfeoluwaAroboto <ifeee@gmail.com>
2022-07-10 23:03:35 +0100

    added commit

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
 let id = document.querySelector(".id")
 let ad = document.querySelector(".advice")
 let btn = document.querySelector(".btn")
-
+let dice = document.querySelector("#roll-dice")
 const apiUrl = "https://api.adviceslip.com/advice"
 
 async function getAdvice(){
     const response  = await fetch(apiUrl)
     const data = await response.json()
-
-    ad.innerHTML = data.slip.advice
-}
-async function getId(){
-    const response  = await fetch(apiUrl)
-    const data = await response.json()
-
     id.innerHTML = "#" + data.slip.id
+    ad.innerHTML = data.slip.advice
 }
 window.addEventListener("DOMContentLoaded", ()=> {
    getAdvice()
-   getId()
-}
-
-)
+})
 btn.addEventListener("click", () => {
     getAdvice()
-    getId()
+ dice.classList.toggle("dice")
+ dice.disabled = true;
 })
